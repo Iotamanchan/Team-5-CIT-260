d5afc4ce97cb3469d72c3ba42dd5e09b18528872
The Whopper <The Whopper@TheWhopper-PC.fios-router.home>
2017-11-17 11:42:24 -0500

    Sell

--- sevenYearsOfPlenty/src/byui/cit260/sevenYearsOfPlenty/control/MapControl.java
+++ sevenYearsOfPlenty/src/byui/cit260/sevenYearsOfPlenty/control/MapControl.java
@@ -10,6 +10,24 @@ package byui.cit260.sevenYearsOfPlenty.control;
  * @author The Whopper
  */
 public class MapControl {
+
+
+    class map {
+        int[][] map = {
+            {1, 2, 3, 4, 5},
+            {6, 7, 8, 9, 10},
+            {11, 12, 13, 14, 15},
+            {16, 17, 18, 19, 20},
+            {21, 22, 23, 24, 25}
+        };
+        int sum = 0; 
+       // for(int i=0, i < 5, i++) {
+         //   for(int j=0, j < 5, j++){
+           //     sum += map[i][j];
+            }
+        }
+    //}
+//} 
+     
     
-    
-}
+
